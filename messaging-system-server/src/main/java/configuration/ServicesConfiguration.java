5359df0d2967b0a2ab170a4f705ce56c42778147
santiago.cuervo <sancramirez@gmail.com>
2019-11-10 16:07:25 -0500

    habilita id en respuesta de servicio rest

---
+++ messaging-system-server/src/main/java/configuration/ServicesConfiguration.java
@@ -0,0 +1,19 @@
+package configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import co.edu.poli.messaging.entities.User;
+
+
+@Configuration
+public class ServicesConfiguration implements RepositoryRestConfigurer {
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(User.class);
+		
+	}
+	
+}
